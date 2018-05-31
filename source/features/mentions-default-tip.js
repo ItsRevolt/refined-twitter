b6bdcd5ac0ec42d4a5f968de5cc501674d37e657
reavolt <revoltsgfx@gmail.com>
2018-05-31 17:08:47 -0400

    Do not show tip if it already exists

--- source/features/mentions-default-tip.js
+++ source/features/mentions-default-tip.js
@@ -1,4 +1,4 @@
-import {h} from 'dom-chef';
+import { h } from 'dom-chef';
 import OptionsSync from 'webext-options-sync';
 var values
 new OptionsSync().getAll().then(options => {
@@ -7,14 +7,15 @@ new OptionsSync().getAll().then(options => {
 
 export const showMentionsTip = async () => {
     if (values.uselessNotifsToggleDisplay == true) {
-	const header = $('.ProfileHeading-toggle')
+        if (document.querySelector('.ProfileHeading-toggle .NotificationsSettingsButton')) return
+        const header = $('.ProfileHeading-toggle')
 
-	header.append(
-		<li class="NotificationsSettingsButton" data-element-term="all_notifications_toggle">
-            <p class="ProfileHeading-toggleLink js-nav" data-nav="all_notifications_toggle" data-next-active-element=".js-nav[data-nav=all_notifications_toggle]">
-                 Only Mentions Are Shown By default <a href="">(?)</a>
-            </p>
-        </li>
-    )
-}
+        header.append(
+            <li class="NotificationsSettingsButton" data-element-term="all_notifications_toggle">
+                <p class="ProfileHeading-toggleLink js-nav" data-nav="all_notifications_toggle" data-next-active-element=".js-nav[data-nav=all_notifications_toggle]">
+                    Only Mentions Are Shown By default <a href="">(?)</a>
+                </p>
+            </li>
+        )
+    }
 }
