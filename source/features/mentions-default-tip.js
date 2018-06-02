40dbe20933f566558c0a1ea0ea949825064e3a3f
reavolt <revoltsgfx@gmail.com>
2018-06-02 18:12:55 -0400

    mentions tip fix

--- source/features/mentions-default-tip.js
+++ source/features/mentions-default-tip.js
@@ -6,8 +6,18 @@ new OptionsSync().getAll().then(options => {
 });
 
 export const showMentionsTip = async () => {
-    if (values.uselessNotifsToggleDisplay == true) {
-        if (document.querySelector('.ProfileHeading-toggle .NotificationsSettingsButton')) return
+    if (values.uselessNotifsToggleDisplay !== true) return
+    if (document.querySelector('.ProfileHeading-toggle .NotificationsSettingsButton')) return
+    const header = $('.ProfileHeading-toggle')
+
+    header.append(
+        <li class="NotificationsSettingsButton" data-element-term="all_notifications_toggle">
+            <p class="ProfileHeading-toggleLink js-nav" data-nav="all_notifications_toggle" data-next-active-element=".js-nav[data-nav=all_notifications_toggle]">
+                Only Mentions Are Shown By default <a href="">(?)</a>
+            </p>
+        </li>
+    )
+    if (document.body.classList.contains('NotificationsPage')) {
         const header = $('.ProfileHeading-toggle')
 
         header.append(
